af537b1ff2182908b341ac153a887294a911ee33
PARTHG0106 <parthgoyal607@gmail.com>
2024-09-23 20:18:27 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',  // Set your MySQL password here
+    host: 'sql303.infinityfree.com',
+    user: 'if0_37369387',
+    password: 'Parthg@123',  // Set your MySQL password here
     database: 'nodetest'
 });
 
